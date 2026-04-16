35a49cdffc5692a13e894a884d2981af325e0e09
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-04-15 21:35:14 -0700

    update

---
+++ pseudo.cpp
@@ -0,0 +1,19 @@
+//Include necessary headers
+using namespace std;
+
+
+//define function to simulate company changes over time
+    //Parameters: map of different companies, number of intervals
+
+//define main
+    //Initialize a map to store company info, each with a value 
+    //that contains an array of three lists for stock price, free 
+    //cash flow, and price to earnings ratio.
+
+    //Open external file to read in data about companies to populate map
+        //error handle
+
+    //Read data from file to populate map
+    //Close file.
+
+    //Begin time based sim for company changes over time period.
